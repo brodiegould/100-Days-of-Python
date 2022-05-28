3a3bff31d12a25f1e13baef43095ab0db3d00cdc
Brodie Gould <brodiegould1@hotmail.com>
2022-05-28 11:33:36 -0700

    fin day1 - command line, i/o, printing

--- Day1--Band-Name-Generator.py
+++ Day1--Band-Name-Generator.py
@@ -1,9 +1,28 @@
-from cgi import test
-from csv import list_dialects
-from turtle import goto
+print('Day 1 - Python Print Function')
+# print("Test with quotations")
+# print('print"(nested)"')
 
+# we can read within a print statement
+# we can also provide a default prompt for the read
+# print("Hello " + input("type your name here :"))
 
-print("This is a test")
-print(100)
-input = input()
-print(input)
+#function to print the length of characters in the input string
+# print(len(input("Enter Name: ")))
+
+# # simple swap
+# a = input("enter value for a: ")
+# b = input("enter value for b: ")
+# print("a before swap:" + a)
+# print("a before swap:" + b)
+# temp = a
+# a=b 
+# b=temp
+# print("a after swap=" + a)
+# print ("b after swap= "+ b)
+
+# Day 1 Band Name Generator
+print("What is the name of the city you grew up in?")
+city = input()
+print("What is the name of your childhood pet?")
+pet = input()
+print("Your band name is :" + city + " " + pet)
