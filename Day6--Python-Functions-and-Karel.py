e6bf06c2da486bd9e2e682ccea6b4393f28a9387
Brodie Gould <brodiegould1@hotmail.com>
2022-06-06 11:19:34 -0700

    while loops, function values, maze exploration

---
+++ Day6--Python-Functions-and-Karel.py
@@ -0,0 +1,34 @@
+#built in functions are defined as function(), where inside the parenthesis is the functions input parameters
+# to create functions we define them, then we can call thme 
+# def function():
+
+def function():
+    print("Hello")
+    print("Goodbye")    
+function()
+
+# tabs are equivalent to 4 spaces
+# in PEP 8 we cant mix spaces for indents and tabs in the same file
+
+# While Loops - used to run until some condition is no longer satisfied
+# unlike for loops, while loops can get caught indefinitely
+# while condition is true:
+    # do stuff
+    
+# program to explore a maze with built in function wall_on_right(), front_is_clear(), at_goal()
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def run():
+#     if wall_on_right() and front_is_clear():
+#         move()
+#     elif not wall_on_right():
+#         turn_right()
+#         move()
+#     else:
+#         turn_left()
+        
+# while not at_goal():
+#     run()
